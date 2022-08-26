0190fa646ce15a85345e6c9a7adb491b28dfb7f1
BHEEM SINGH MEENA <111851919+meena0107@users.noreply.github.com>
2022-08-27 01:22:03 +0530

    Add files via upload

---
+++ PROJECT ANALYSIS INSIGHTS.sql
@@ -0,0 +1,60 @@
+PROJECT ANALYSYS INSIGHTS
+-- review the table
+
+-- Analysis 01- Total episodes telecasted into SharkTankIndia
+
+
+-- Analysis 02- Pitches that had pitched onto the SharkTankIndia
+
+
+-- Analysis 03- Pitches Converted(gets funding from SharkTankIndia) in Percentage
+
+
+-- Analysis 04- Total males participated in SharkTankIndia
+
+
+-- Analysis 05- Total female partcipated into SharkTankIndia
+
+
+-- Analysis 06- Gender ratio in Shark Tank India
+
+
+-- Analysis 07- Total Invested Amount into the Show
+
+
+-- Analysis 08- AVG Equity Taken by the Sharks
+
+
+-- Analysis 09- Highest deal done into SharkTankIndia
+
+
+-- Analysis 10- Highest equity taken by the sharks
+
+
+-- Analysis 11- Startups having at least women
+
+
+-- Analysis 12- Pitches converted having atleast one women
+
+
+-- Analysis 13- AVG team Members 
+
+
+-- Analysis 14- Amount invested per deal by SharkTankIndia
+
+
+-- Analysis 15- AVG age group of contestants
+
+
+-- Analysis 16- Location group of contestent
+
+
+-- Analysis 17- Sector group of contestent
+
+
+-- Analysis 18- Partner Deals
+
+
+-- Analysis 19- Which is the startup in which the highest amount has been invested in each domain/sector
+
+
